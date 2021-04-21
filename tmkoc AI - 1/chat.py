7742669ed8c002c525650cd236461f79a91ecc40
Kanishk Sharma <61106875+kanishksh4rma@users.noreply.github.com>
2021-04-21 21:55:29 +0000

    Jethiya AI all versions uploaded.
    25-32 has good ans though vocab size, input parameter size and epochs' hyperparameter tuning can make make it much better.

---
+++ tmkoc AI - 1/chat.py
@@ -0,0 +1,63 @@
+import json
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+
+import colorama
+colorama.init()
+from colorama import Fore, Style, Back
+
+import random
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+
+def chat():
+    # load trained model
+    model = keras.models.load_model('chat_model')
+
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+
+    while True:
+        print(Fore.LIGHTBLUE_EX + "User: " + Style.RESET_ALL, end="")
+        inp = input()
+        inp = inp.lower()
+        if inp.lower() == "quit":
+            break
+
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+        #print(result)
+        print('max : ',np.argmax(result))
+        excuseme = ['kuch smjh me aaye aisa bol?','Mehta saheb, kitne kathin words ka use krte ho. yeh daya ko smjh me aaye aisa kuch bolo.','le phir se english']
+        proper=True
+
+
+        if np.argmax(result)<0:
+            print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(excuseme))
+            proper=False
+        else:
+            proper = True
+
+        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+        if proper:
+            for i in data['intents']:
+                if i['tag'] == tag:
+                    print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+
+        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+
+print(Fore.YELLOW + "Start messaging with the bot (type quit to stop)!" + Style.RESET_ALL)
+chat()
